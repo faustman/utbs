85e8d017b4420b0d0a502f004a8ee8e97d928ed5
Andrey Titar <faust@dot.com.ua>
2013-10-21 23:16:13 +0300

    Add webmock to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,9 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
+# Stub for requests
+require 'webmock/minitest'
+
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
