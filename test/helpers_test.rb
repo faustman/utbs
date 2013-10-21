c174b99c5c2aa067ecce6818bcc84c62df6a8d07
Andrey Titar <faust@dot.com.ua>
2013-10-21 22:35:07 +0300

    Refactor tests

--- test/helpers_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class HelpersTest < ActionView::TestCase
-  test "have utbs_links helper" do
-    assert_respond_to view, :utbs_links, "Wooops. Rails cant't see the helper"
-  end
-end
-
